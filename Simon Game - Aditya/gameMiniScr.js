edaa801a26da989682a0764159c8a8cfd96ecf7b
adityab-28 <72013227+adityab-28@users.noreply.github.com>
2021-06-02 21:44:02 +0530

    Update gameMiniScr.js

--- Simon Game - Aditya/gameMiniScr.js
+++ Simon Game - Aditya/gameMiniScr.js
@@ -6,8 +6,8 @@ if (firstTime) {  $(".instructions").animate({  opacity: "90%"  }, 500);
 } $("#ins-btn").click(() => {  if (firstTime) {  $(".instructions").fadeOut(500); $("#ins-btn").text("Close");
     // $(".btn, .start-btn, #level-title").animate({opacity:1},100);
   $(".info-section").animate({  opacity: 0.93  }, 500);  firstTime = false;  } else {  $(".instructions").slideToggle(500);  }});
-
 var clickInfo = false; $(".info-btn, #about-btn").click(function() {
+  $("#about-btn").text("Close");
 if (clickInfo) {
   $(".info-section").animate({  opacity: 1,  left: "-=15",  top: "+=35",  height: "toggle"  }, 200);  clickInfo = false;  }
 else {  $(".info-section").animate({  opacity: 0,  top: "-=35",  left: "+=15",  height: "toggle"  }, 200);  clickInfo = true;  }
